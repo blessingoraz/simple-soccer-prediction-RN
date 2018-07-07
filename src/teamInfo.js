ea2f432e76a5584d015ccd7608341ebfb6d3cd70
Blessing Orazulume <orazulumeblessing@gmail.com>
2018-07-07 21:15:57 +0100

    Updated teaminfo object with id

--- src/teamInfo.js
+++ src/teamInfo.js
@@ -1,24 +1,31 @@
 const teamInfo = [{
+    id: 1,
     name: 'Arsenal',
     imagePath: require('../assets/Arsenal.png')
 }, {
+    id: 2,
     name: 'ManUnited',
-    imagePath: '../assets/ManUnited.png'
+    imagePath: require('../assets/ManUnited.png')
 }, {
+    id: 3,
     name: 'Chelsea',
-    imagePath: '../assets/Chelsea.png'
+    imagePath: require('../assets/Chelsea.png')
 }, {
+    id: 4,
     name: 'Liverpool',
-    imagePath: '../assets/Liverpool.png'
+    imagePath: require('../assets/Liverpool.png')
 }, {
+    id: 5,
     name: 'Barca',
-    imagePath: '../assets/Barca.png'
+    imagePath: require('../assets/Barca.png')
 }, {
+    id: 6,
     name: 'Bayern',
-    imagePath: '../assets/Bayern.png'
+    imagePath: require('../assets/Bayern.png')
 }, {
+    id: 8,
     name: 'StokeCity',
-    imagePath: '../assets/StokeCity.jpg'
+    imagePath: require('../assets/StokeCity.jpg')
 }];
 
 export default teamInfo;
